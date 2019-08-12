b2679029d3de6b740b2e90d28565ed6d8678ecc3
Yannick Fricke <yannickfricke@googlemail.com>
2019-08-12 18:03:13 +0200

    Fixed git auto pusher formulae

--- git-auto-pusher.rb
+++ git-auto-pusher.rb
@@ -4,7 +4,7 @@ class GitAutoPusher < Formula
   url "https://github.com/YannickFricke/git-auto-pusher/archive/v1.0.0.tar.gz"
   sha256 "cb79a46553a02a7d22a7cc77a186534adf91afda490d946dd895a6f797adecec"
 
-  dependency git => :optional
+  dependency "git" => :optional
 
   def install
     bin.install "git_auto_push"
