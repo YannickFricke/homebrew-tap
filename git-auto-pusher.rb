bca83d585055ad99ccaf19a50ef16fa17b5b6cb7
Yannick Fricke <yannickfricke@googlemail.com>
2019-08-12 17:58:18 +0200

    Added the formulae for the git auto pusher

---
+++ git-auto-pusher.rb
@@ -0,0 +1,12 @@
+class GitAutoPusher < Formula
+  desc "Pushes new commits to existing remotes"
+  homepage "https://github.com/YannickFricke/git-auto-pusher"
+  url "https://github.com/YannickFricke/git-auto-pusher/archive/v1.0.0.tar.gz"
+  sha256 "cb79a46553a02a7d22a7cc77a186534adf91afda490d946dd895a6f797adecec"
+
+  dependency git => :optional
+
+  def install
+    bin.install "git_auto_push"
+  end
+end
